20bc610ac00d98cdb19ddf7a57675e55642fdf53
Roy Moore <roy@moore.co.il>
2019-03-23 15:23:06 +0200

    use argparse

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-
+"""
+A small web service for printing similar words in the English language.
+"""
 __author__ = "Roy Moore"
 __license__ = "MIT"
 __version__ = "1.0.0"
 
-from flask import Flask
+import argparse
 
-APP_PORT = 8000
-APP_DEBUG = True
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -18,4 +19,9 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=APP_DEBUG, host='0.0.0.0', port=APP_PORT)
+    parser = argparse.ArgumentParser(description='Print Similar Web Service')
+    parser.add_argument("-d", "--debug", action="store_true", default=False, help="enable debug mode")
+    parser.add_argument("-n", "--host", action="store", type=str, default="0.0.0.0", help="hostname to listen on")
+    parser.add_argument("-p", "--port", action="store", type=int, default=8000, help="port of the webserver")
+    args = parser.parse_args()
+    app.run(debug=args.debug, host=args.host, port=args.port)
