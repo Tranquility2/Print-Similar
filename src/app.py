745b6bd133a819e74f492ed41d694d67bd6e0c48
Roy Moore <roy@moore.co.il>
2019-03-24 22:31:58 +0200

    refactor to create Server

--- src/app.py
+++ src/app.py
@@ -5,16 +5,17 @@ A small web service for printing similar words in the English language.
 import json
 import sys
 import time
+import argparse
+import logging
+
 from collections import defaultdict
+from flask import Flask, request, g
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 
 __author__ = "Roy Moore"
 __license__ = "MIT"
 __version__ = "1.0.0"
-
-import argparse
-import logging
-
-from flask import Flask, request, g
 
 # noinspection SpellCheckingInspection
 logging.basicConfig(level=logging.DEBUG,
@@ -29,7 +30,7 @@ class FancyDictionary:
         :param str path: path of source dictionary
         """
         self.logger = logger
-        self._data = self._load_data_file(path)
+        self._data, self.total_words = self._load_data_file(path)
 
     @staticmethod
     def _sort_word(word):
@@ -59,11 +60,11 @@ class FancyDictionary:
         """
         start_time = time.time()
         data = defaultdict(list)
-        words = 0
+        total_words = 0
 
         with open(path, "r") as fileHandler:
             for line in fileHandler:
-                words += 1
+                total_words += 1
                 word = line.strip()  # Need to stripe as each line (except last one) will contain new line character
                 sorted_word = self._sort_word(word)  # get the sorted version of the word
                 data[sorted_word] += [word]  # Insert the data to the DB
@@ -71,11 +72,93 @@ class FancyDictionary:
         end_time = time.time()
 
         self.logger.info("DB loaded successfully (loaded in %.5fs)", end_time - start_time)
-        self.logger.debug(f"Words count = {words}, "
+        self.logger.debug(f"Words count = {total_words}, "
                           f"DB Keys = {len(data.keys())}, "
                           f"DB Values = {sum([len(data[x]) for x in data if isinstance(data[x], list)])}")
 
-        return data
+        return data, total_words
+
+
+class StreamingMovingAverage:
+    def __init__(self, window_size):
+        self.window_size = window_size
+        self.values = []
+        self.sum = 0
+
+    def process(self, value):
+        print(value)
+        self.values.append(value)
+        self.sum += value
+        if len(self.values) > self.window_size:
+            self.sum -= self.values.pop(0)
+        print(float(self.sum), len(self.values))
+        return int(float(self.sum) / len(self.values))
+
+
+class Server:
+    @dataclass_json
+    @dataclass
+    class StatInfo:
+        totalWords: int
+        totalRequests: int = 0
+        avgProcessingTimeNs: int = 0
+
+    def __init__(self, debug, host, port, logger):
+        self.debug = debug
+        self.host = host
+        self.port = port
+        self.logger = logger
+        self.db = FancyDictionary(args.file, logger)
+        self.stat_info = self.StatInfo(totalWords=self.db.total_words)
+        self.avg_time_calc = StreamingMovingAverage(window_size=10)
+
+    BASE_URL = "/api/"
+    VERSION_URL = BASE_URL + 'v1/'
+    PING_URL = VERSION_URL + 'ping'
+    SIMILAR_URL = VERSION_URL + 'similar'
+    STATS_URL = VERSION_URL + 'stats'
+
+    def run(self):
+        app = Flask(__name__)
+
+        @app.before_request
+        def before_request():
+            g.request_start_time = time.time()
+            g.request_time = lambda: "%.f" % ((time.time() - g.request_start_time) * 100000000)  # Convert to nano
+
+        @app.route(self.PING_URL, methods=['GET'])
+        def get_test():
+            """
+            Test the server
+            """
+            return "pong"
+
+        @app.route(self.SIMILAR_URL, methods=['GET'])
+        def get_similar():
+            """
+            Returns all words in the dictionary that has the same permutation as the word
+            :rtype: str
+            """
+            self.StatInfo.totalRequests += 1
+            requested_word = request.args.get('word')
+            result_dict = dict()
+            result_dict['similar'] = self.db.check(requested_word)  # TODO: change to result object?
+            result_json = json.dumps(result_dict)
+            request_time = int(g.request_time())
+            log.debug(f"request time {request_time}ns")
+            self.stat_info.avgProcessingTimeNs = self.avg_time_calc.process(request_time)
+
+            return result_json
+
+        @app.route(self.STATS_URL, methods=['GET'])
+        def get_stats():
+            """
+            Return general statistics
+            :rtype: str
+            """
+            return self.stat_info.to_json()
+
+        app.run(debug=self.debug, host=self.host, port=self.port)
 
 
 if __name__ == '__main__':
@@ -88,52 +171,5 @@ if __name__ == '__main__':
 
     log = logging.getLogger(__name__)
 
-    DB = FancyDictionary(args.file, log)
-
-    app = Flask(__name__)
-
-    BASE_URL = "/api/"
-    VERSION_URL = BASE_URL + 'v1/'
-    PING_URL = VERSION_URL + 'ping'
-    SIMILAR_URL = VERSION_URL + 'similar'
-    STATS_URL = VERSION_URL + 'stats'
-
-
-    @app.before_request
-    def before_request():
-        g.request_start_time = time.time()
-        g.request_time = lambda: "%.5fs" % (time.time() - g.request_start_time)
-
-
-    @app.route(PING_URL, methods=['GET'])
-    def get_test():
-        """
-        Test the server
-        """
-        return "pong"
-
-
-    @app.route(SIMILAR_URL, methods=['GET'])
-    def get_similar():
-        """
-        Returns all words in the dictionary that has the same permutation as the word
-        :rtype: str
-        """
-        requested_word = request.args.get('word')
-        result_dict = dict()
-        result_dict['similar'] = DB.check(requested_word)  # TODO: change to result object?
-        result_json = json.dumps(result_dict)
-        log.debug(f"request time {g.request_time()}")
-
-        return result_json
-
-
-    @app.route(STATS_URL, methods=['GET'])
-    def get_stats():
-        """
-        Return general statistics
-        :rtype: str
-        """
-        return "stats"
-
-    app.run(debug=args.debug, host=args.host, port=args.port)
+    server = Server(debug=args.debug, host=args.host, port=args.port, logger=log)
+    server.run()
