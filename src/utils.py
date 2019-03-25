14cf391603d509b82cf5b17890da07426f3cb210
Roy Moore <roy@moore.co.il>
2019-03-25 23:36:38 +0200

    make StreamingMovingAverage mote thread safe

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,17 @@
+import threading
+
+
 class StreamingMovingAverage:
     def __init__(self, window_size):
+        self.moving_average_lock = threading.Lock()
         self.window_size = window_size
         self.values = []
         self.sum = 0
 
     def process(self, value):
-        self.values.append(value)
-        self.sum += value
-        if len(self.values) > self.window_size:
-            self.sum -= self.values.pop(0)
-        return int(float(self.sum) / len(self.values))
+        with self.moving_average_lock:
+            self.values.append(value)
+            self.sum += value
+            if len(self.values) > self.window_size:
+                self.sum -= self.values.pop(0)
+            return int(float(self.sum) / len(self.values))
